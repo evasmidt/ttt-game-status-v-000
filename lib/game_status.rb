2049c3b22127a4f6d375c1baaaf1a3656aea06d4
Eva Smidt <shevangeline@gmail.com>
2018-02-04 02:06:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,7 @@ def won?(board)
 
    position_1 == "X" && position_2 == "X" && position_3 =="X" || position_1 == "O" && position_2 == "O" && position_3 =="O"
 
-    #win_combination
-
+  
   end
 
     #binding.pry
